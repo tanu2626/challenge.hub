74d75107859b7e7c2400c8b73ff9bd3d43f6ea5f
Tanu <tanuuu.21@gmail.com>
2016-12-27 21:31:26 +1100

    changes in layouts and apllication.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+
+
+def contact_us
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'menu/index'
+
  root 'home#index'
+
+ get 'contact_us' => 'home#contact_us'
+ get 'menu' => 'menu#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
